5611422a1a770a81195379932d7d9252f94c5200
Kurtlin2012 <58333746+Kurtlin2012@users.noreply.github.com>
2020-07-16 15:10:15 +0800

    Update tools.py

--- preprocessing/tools.py
+++ preprocessing/tools.py
@@ -5,13 +5,14 @@ Created on Tue Jun 30 13:17:46 2020
 @author: Ching-Ting Kurt Lin
 """
 
-import numpy as np
 
 def categorise_prediction(max_ids, category):
     
     """
     This function is to prevent if the voxel was classified into multiple channels. 
     """
+    
+    import numpy as np
     
     output = np.zeros(max_ids.shape)
     output[(max_ids == category)] = 1
