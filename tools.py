ad6314c740fffd5eaccd45f10a163078c94b5e29
Kurtlin2012 <58333746+Kurtlin2012@users.noreply.github.com>
2020-09-14 16:45:23 +0800

    Create tools.py

---
+++ tools.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 30 13:17:46 2020
+
+@author: Ching-Ting Kurt Lin
+"""
+
+def categorise_prediction(max_ids, category):
+    
+    """
+    This function is to prevent if the voxel was classified into multiple channels. 
+    """
+    
+    import numpy as np
+    
+    output = np.zeros(max_ids.shape)
+    output[(max_ids == category)] = 1
+    return output
